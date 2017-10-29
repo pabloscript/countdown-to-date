5eeea40cf6ed0a25a6c064660e8dfc0ff881c436
Pawel Kozicki <pe.kozicki@gmail.com>
2017-10-29 21:37:44 +0100

    Add structure, state, setState methods

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,36 @@
 import React, { Component } from 'react';
+import './App.css';
 
 class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            deadline: "December 25, 2017",
+            newDeadline: ""
+        }
+    }
+
+    changeDeadline() {
+        this.setState({deadline: this.state.newDeadline})
+    }
+
     render() {
         return (
-        <div>Hello World!</div>
+        <div className="App">
+            <div className="App-title">Countdown to {this.state.deadline}</div>
+            <div>
+                <div className="Clock-days">14 days</div>
+                <div className="Clock-hours">30 hours</div>
+                <div className="Clock-minutes">15 minutes</div>
+                <div className="Clock-seconds">20 seconds</div>
+            </div>
+            <div>
+                <input placeholder="new date" onChange={e => this.setState({newDeadline: e.target.value})}/>
+                <button onClick={() => this.changeDeadline()}>
+                    Submit
+                </button>
+            </div>
+        </div>
         );
     }
 }
